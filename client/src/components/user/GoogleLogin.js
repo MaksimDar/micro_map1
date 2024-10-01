4f58c6e8979bae6534406a72027781bb84a6b1c0
MaksimDar <dovhusha.m@gmail.com>
2024-10-02 00:37:23 +0200

    Loganisation
    start point of google loganisation process

--- client/src/components/user/GoogleLogin.js
+++ client/src/components/user/GoogleLogin.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Google } from '@mui/icons-material';
 import { Button } from "@mui/material";
+import { useValue } from "../../context/ContextProvider";
+
 
 const GoogleLogin = () => {
+    const { dispatch } = useValue();
+    const [disabled, setDisabled] = useState(false);
+
+    const handleResponse = (response) => {
+        console.log(response);
+    }
+    const handleGoogleLogin = () => {
+        setDisabled(true);
+        try {
+            window.google.accounts.id.initialize({
+                client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+                callback: handleResponse
+            });
+            window.google.accounts.id.prompt((notification) => {
+                if (notification.isNotDisplayed()) {
+                    throw new Error('Try to clear the cookies or try again');
+                };
+                if (notification.isSkippedMoment() || notification.isDismissedMoment()) {
+                    setDisabled(false);
+                }
+            })
+
+        } catch (error) {
+            dispatch({ type: 'UPDATE_ALERT', payload: { open: true, severity: 'error', message: error.message } });
+            console.log(error);
+        }
+    }
     return (
         <Button
             variant="outlined"
-            startIcon={<Google />}>
+            startIcon={<Google />} disabled={disabled} onClick={handleGoogleLogin}>
             Log in with Google
         </Button>
     )
