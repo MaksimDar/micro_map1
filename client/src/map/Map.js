5d2f34b34a4a0a0a268bb08b862c76430d1ccf8e
MaksimDar <dovhusha.m@gmail.com>
2024-12-20 21:13:52 +0100

    Search Box
    The search box (basic filter) is done

--- client/src/map/Map.js
+++ client/src/map/Map.js
@@ -3,6 +3,8 @@ import { SearchBox } from "@mapbox/search-js-react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Map } from "./Map.styled";
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 const accessToken = "pk.eyJ1IjoibWFrc3ltMTgiLCJhIjoiY2x5c280eDF2MDkweDJrc2N3a2R5c3l1cyJ9.70P7zxzTa0uSxIpnkz7YeA";
 
@@ -16,7 +18,6 @@ export default function MapWithGeocoder() {
     useEffect(() => {
         const fetchLocations = async () => {
             try {
-                // const response = await fetch('http://localhost:5001/locations');
                 const response = await fetch('https://micro-map1.onrender.com/locations');
                 const data = await response.json();
                 setLocations(data);
@@ -38,18 +39,27 @@ export default function MapWithGeocoder() {
             zoom: 4.3,
         });
 
+        mapInstanceRef.current = map;
+
+        const geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl,
+            language: "en"
+        });
+        map.addControl(geocoder);
+
         map.on("load", () => {
             setMapLoaded(true);
-
             locations.forEach((location) => {
                 const { longitude, latitude, name, streetViewLink } = location;
+
                 const marker = new mapboxgl.Marker()
                     .setLngLat([longitude, latitude])
                     .addTo(map);
 
                 const popup = new mapboxgl.Popup({ offset: 25 })
                     .setHTML(`
-                        <h3>${location.name}</h3>
+                        <h3>${name}</h3>
                         <p><a href="${streetViewLink}" target="_blank">View in ${name}</a></p>
                     `);
 
